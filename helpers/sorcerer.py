a715c9cd589bd8463a9d3c8b6b687a2669b09750
LouisBertin <bertin.louis7@gmail.com>
2018-09-18 10:21:55 -0400

    get_level method

--- helpers/sorcerer.py
+++ helpers/sorcerer.py
@@ -9,14 +9,31 @@ def get_all_sorcerer():
 
     title = soup.find("div",{'class':'heading'}).find('p').get_text()
     school = get_school(soup.find("p",{'class':'SPDet'}))
+    level = get_level(soup.find("p",{'class':'SPDet'}))
+    casting_time = get_casting_time(soup.findAll("p", {"class": "SPDet"})[1])
 
-    return school
+    return level
 
 
-def get_school(school_string):
+def get_school(school):
     start = '<b>School</b>'
     end = '<b>Level</b>'
-    s = str(school_string)
+    s = str(school)
     school = s[s.find(start)+len(start):s.rfind(end)]
+    return school.strip().replace(";", "")
 
-    return school.strip()
+
+def get_level(level):
+    start = 'sorcerer/wizard'
+    end = ','
+    s = str(level)
+    sorcerer_level = s[s.find(start)+len(start):s.rfind(end)]
+
+    if "sorcerer/wizard" not in s:
+        return 0
+
+    return sorcerer_level.strip()[:1]
+
+
+def get_casting_time(casting_time):
+    return casting_time.get_text()
