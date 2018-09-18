612ebbea747fe2395675fa451c5f402df5e17cb9
LouisBertin <bertin.louis7@gmail.com>
2018-09-18 09:44:05 -0400

    build get_school method

--- helpers/sorcerer.py
+++ helpers/sorcerer.py
@@ -5,6 +5,18 @@ from requests import get
 def get_all_sorcerer():
     url = "http://www.dxcontent.com/SDB_SpellBlock.asp?SDBID=1000"
     r = get(url).text
-    soup = BeautifulSoup(r, 'html.parser')
+    soup = BeautifulSoup(r, 'html.parser').find("div",{'class':'SpellDiv'})
+
     title = soup.find("div",{'class':'heading'}).find('p').get_text()
-    return title
+    school = get_school(soup.find("p",{'class':'SPDet'}))
+
+    return school
+
+
+def get_school(school_string):
+    start = '<b>School</b>'
+    end = '<b>Level</b>'
+    s = str(school_string)
+    school = s[s.find(start)+len(start):s.rfind(end)]
+
+    return school.strip()
